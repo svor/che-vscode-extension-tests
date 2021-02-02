c8bc76bb8c5e4df77772489632aee64c345a5ef3
svor <vsvydenk@redhat.com>
2021-02-02 14:43:34 +0200

    update
    Signed-off-by: svor <vsvydenk@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2020 Red Hat, Inc.
+ * Copyright (c) 2021 Red Hat, Inc.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -36,7 +36,6 @@ export function start(context: theia.PluginContext): void {
                     return e(err);
                 }
 
-                console.log(" ------ Find tests files -------");
                 console.log("Found: ");
                 console.log(files);
 
--- testWorkspace/src/test/java/org/my/sample/NativeMySampleIT.java
+++
@@ -1,9 +0,0 @@
-package org.my.sample;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMySampleIT extends MySampleTest {
-
-    // Execute the same tests but in native mode.
-}
